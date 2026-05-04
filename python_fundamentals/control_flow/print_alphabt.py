f995b3413996e5189ab865cb136522e5d1ac125e
olium <jeremie.rouxel@outlook.fr>
2026-05-05 00:25:15 +0200

    .

--- python_fundamentals/control_flow/print_alphabt.py
+++ python_fundamentals/control_flow/print_alphabt.py
@@ -1,4 +1,4 @@
 #!/usr/bin/env python3
 for alphabet in (range(97, 123)):
     if alphabet != 101 and alphabet != 113:
-        print(chr(alphabet), end='')
+        print(chr(alphabet), end='\n' if alphabet == 122 else '')
