8058eedc360ee6e2a082e757ad9ff55e58acc532
olium <jeremie.rouxel@outlook.fr>
2026-05-05 15:54:53 +0200

    .

--- python_fundamentals/control_flow/print_comb3.py
+++ python_fundamentals/control_flow/print_comb3.py
@@ -1,4 +1,5 @@
 #!/usr/bin/env python3
 for digit in (range(0, 10)):
     for digit2 in (range(digit+1, 10)):
-        print('{:02d}'.format(digit*10+digit2), end=', ' if digit*10+digit2 != 89 else '\n')
+        separateur = ', ' if digit*10+digit2 != 89 else '\n'
+        print('{:02d}'.format(digit*10+digit2), end=separateur)
