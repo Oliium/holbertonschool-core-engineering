b6fc5f85c3b88835c90618bee89d7e4f886447e2
olium <jeremie.rouxel@outlook.fr>
2026-05-11 22:10:27 +0200

    .

---
+++ python_fundamentals/exception_handling/safe_print_list.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python3
+def safe_print_list(my_list=[], x=0):
+    count = 0
+
+    for elements in range(x):
+        try:
+            print("{}".format(my_list[elements]), end="")
+            count += 1
+        except IndexError:
+            break
+
+    print()
+    return count
