b9424332a355342d79cce3f54fae39405faa4237
olium <jeremie.rouxel@outlook.fr>
2026-05-12 21:55:40 +0200

    .

--- python_oop/classes_and_object_model/6-square.py
+++ python_oop/classes_and_object_model/6-square.py
@@ -35,7 +35,7 @@ class Square:
                 len(value) != 2 or
                 not all(isinstance(n, int) for n in value) or
                 not all(n >= 0 for n in value)):
-            raise TypeError("position must be a tuple of 2 positive integer")
+            raise TypeError("position must be a tuple of 2 positive integers")
         self.__position = value
 
     def area(self):
