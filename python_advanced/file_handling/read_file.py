7f12591d1dd6e6a1e95a4fdae0c1096064e166df
olium <jeremie.rouxel@outlook.fr>
2026-05-26 14:46:54 +0200

    .

---
+++ python_advanced/file_handling/read_file.py
@@ -0,0 +1,7 @@
+"""Module for reading a text file and printing it to stdout."""
+
+
+def read_file(filename=""):
+    """Read a UTF-8 text file and print its content to stdout."""
+    with open(filename, 'r', encoding='utf-8') as file:
+        print(file.read(), end="")
